74834f3a1ce477fd397de3b50cc0f0e6f0d34d62
Kathan <kathanmodi2001@gmail.com>
2026-04-27 13:19:03 +0530

    feat: initialize spring boot implementation

---
+++ bookmark-spring-boot/src/main/java/bookmark_spring_boot/BookmarkSpringBootApplication.java
@@ -0,0 +1,13 @@
+package bookmark_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmarkSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmarkSpringBootApplication.class, args);
+	}
+
+}
---
+++ bookmark-spring-boot/src/test/java/bookmark_spring_boot/BookmarkSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package bookmark_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmarkSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
